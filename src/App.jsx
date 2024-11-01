1fd23e4a94e982d55c82d6bfb827ad0fbb534be9
Gusta346 <gustavoribeiro4523@gmail.com>
2024-11-01 08:44:18 -0300

    Criando Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-
+import { Outlet } from 'react-router-dom'
+import Side from './components/SideBar/Side'
 
 function App() {
 
 
   return (
     <>
-      
+      <Side/>
+      <Outlet/>
     </>
   )
 }
